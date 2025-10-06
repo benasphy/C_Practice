c657a6023927cd6eb71ffbba728290b968a6dc38
benasphy <binidani1903@gmail.com>
2025-10-06 11:19:27 +0300

    malloc example

--- mallocex.c
+++ mallocex.c
@@ -15,16 +15,13 @@ int main(){
         return 1;
     }
 
-    for (int i = 0; i < number; i++){
-        printf("Enter grade #%d: ", i+1);
-        scanf(" %c", &grades[i]);
-    }
 
     for (int i = 0; i < number; i++){
-        printf("%c ", grades[i]);
+        printf("%d", grades[i]);
     }
 
 
 
     free(grades);
+    grades = NULL; //avoids dangling pointer
 }
