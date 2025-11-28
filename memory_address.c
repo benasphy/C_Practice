744587a8ebbf8c0eb42a2f7b13b2b1d089285e77
benasphy <binidani1903@gmail.com>
2025-11-28 21:36:21 +0300

    Memory address

---
+++ memory_address.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10; // Declare and initialize variable a
+
+    // Print the memory address of a
+    printf("The memory address of a is: %p\n", (void*)&a);
+
+    return 0;
+}
